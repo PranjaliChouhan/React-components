31ec0fe79a5aee06f0b449e39d5e942cc4a61e1a
PranjaliChouhan <pranjalichouhan4@gmail.com>
2023-07-08 10:16:26 +0530

    initial

---
+++ components/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Button from "./Functional";
+import  Counter from "./Class";
+function App() {
+  return (
+    <div>
+      <Button/>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
